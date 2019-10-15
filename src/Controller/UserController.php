75444469e42f3146ecdab8d0eb1ac759bc12e9ec
Afonso de Mori <afonso.mori@gmail.com>
2019-10-15 17:47:32 +0200

    Add POST users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -28,5 +34,29 @@ class UserController extends AbstractController
     public function findOne(int $id, UserRepository $userRepository): JsonResponse
     {
         return $this->json($userRepository->find($id));
+    }
+
+    /**
+     * @Route("/users", methods={"POST"})
+     * @param Request $request
+     * @param UserRepository $userRepository
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function create(Request $request, UserRepository $userRepository, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent());
+        $user = (new User())
+            ->setEmail($data->email)
+            ->setName($data->name)
+            ->setSurname($data->surname)
+            ->setPassword(password_hash($data->password, PASSWORD_ARGON2I))
+            ->setBirthdate(new \DateTime($data->birthdate));
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([], 204);
     }
 }
